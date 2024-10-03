00ba57b4e36c04ab4ffce709f6def978ad563ef3
Mark <doylethecat@gmail.com>
2024-10-03 16:32:44 -0400

    [CODE] Q2

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from scipy.integrate import solve_ivp
 
 # following tutorial
 # https://www.youtube.com/watch?v=MM3cBamj1Ms
+#
+# Book if you need help
+# https://pythonnumericalmethods.studentorg.berkeley.edu/notebooks/chapter22.06-Python-ODE-Solvers.html
+
 
 
 # coupled first_order ODE
@@ -28,7 +32,7 @@ from scipy.integrate import solve_ivp
 # S_0 = (y1_0, y2_0)
 
 
-
+# ----------
 
 # Second Order ODE
 # say x'' = -x'^2 + sin(x)
@@ -48,11 +52,25 @@ v_0 = 5
 S_0 = (x_0, v_0)
 
 
+def dSdt(t, S):
+    x1, x2 = S
+    k1 = 12
+    k2 = 2
+    return [-x1 - k1*x2,
+            k2*x1 - x1**2 - x2]
+x1_0 = 0.1
+x2_0 = 0
+S_0 = (x1_0, x2_0)
+
+
+
 
 
 if __name__ == "__main__":
     # setup what x will be!
-    x = np.linspace(0, 1, 100)
+    # x = np.linspace(0, 1, 100)
+    t = np.linspace(0, 1, 100)
+
 
     # ODE Int more oldschool, uses Fortran
     # solve_ivp more flexible
@@ -60,10 +78,14 @@ if __name__ == "__main__":
     # sol_m1 = odeint(dvdt, y0=v0, t=t, tfirst=True)
     # sol_m2 = solve_ivp(dvdt, t_span=(0, max(t)), y0=[v0], t_eval=t)
 
-    sol = odeint(dSdx, y0=S_0, t=x, tfirst=True)
+    # sol = odeint(dSdx, y0=S_0, t=x, tfirst=True)
+    sol = solve_ivp(dSdt, t_span=(0, max(t)), y0=S_0, t_eval=t)
 
-    x_sol = sol.T[0]
-    v_sol = sol.T[1]
+    print(sol.y)
+
+    x1_sol = sol.y[0]
+    x2_sol = sol.y[1]
+    t = sol.t
 
     # first parameter is velocity
     # print(sol_m1.T[0])
@@ -73,8 +95,8 @@ if __name__ == "__main__":
     # v_sol_m1 = sol_m1.T[0]
     # v_sol_m2 = sol_m2.y[0]
 
-    plt.plot(x, x_sol)
-    plt.plot(x, v_sol)
+    plt.plot(t, x1_sol)
+    plt.plot(t, x2_sol)
     # plt.ylabel('$v(t)$', fontsize=22)
     # plt.xlabel('$t$', fontsize=22)
     plt.show()
