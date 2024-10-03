0a746c22359f9f180c4fd7ccb1aaa3c822342934
Mark <doylethecat@gmail.com>
2024-10-03 15:57:29 -0400

    [CODE] 2nd order ODE
    Included code tutorial for 2nd order ODE

--- main.py
+++ main.py
@@ -8,12 +8,6 @@ from scipy.integrate import solve_ivp
 # https://www.youtube.com/watch?v=MM3cBamj1Ms
 
 
-def dvdt(t, v):
-    return 3*v**2 - 5
-
-v0 = 0
-
-
 # coupled first_order ODE
 #y1' = y1 + y2^2 + 3x
 #y2' = 3y1 + y2^3 - cos(x)
@@ -25,14 +19,33 @@ v0 = 0
 # dS/dx = (dy1/dx, dy2/dx)
 # just want our vector from its derivative!
 
-def dSdx(x, S):
-    y1, y2 = S
-    return [y1 + y2**2 + 3*x,
-            3*y1 + y2**3 - np.cos(x)]
-y1_0 = 0
-y2_0 = 0
-S_0 = (y1_0, y2_0)
+# def dSdx(x, S):
+#     y1, y2 = S
+#     return [y1 + y2**2 + 3*x,
+#             3*y1 + y2**3 - np.cos(x)]
+# y1_0 = 0
+# y2_0 = 0
+# S_0 = (y1_0, y2_0)
 
+
+
+
+# Second Order ODE
+# say x'' = -x'^2 + sin(x)
+#
+# We will convert 2nd order ODE to first order!
+# x' = v
+# v' = -v^2 + sin(x)
+# boom! we've got two equations now!
+# We know S = (x', v') = (v, -v^2 + sin(x))
+def dSdx(x, S):
+    x, v = S
+    return [v,
+            -v**2 + np.sin(x)]
+
+x_0 = 0
+v_0 = 5
+S_0 = (x_0, v_0)
 
 
 
@@ -41,13 +54,16 @@ if __name__ == "__main__":
     # setup what x will be!
     x = np.linspace(0, 1, 100)
 
+    # ODE Int more oldschool, uses Fortran
+    # solve_ivp more flexible
+
     # sol_m1 = odeint(dvdt, y0=v0, t=t, tfirst=True)
     # sol_m2 = solve_ivp(dvdt, t_span=(0, max(t)), y0=[v0], t_eval=t)
 
     sol = odeint(dSdx, y0=S_0, t=x, tfirst=True)
 
-    y1_sol = sol.T[0]
-    y2_sol = sol.T[1]
+    x_sol = sol.T[0]
+    v_sol = sol.T[1]
 
     # first parameter is velocity
     # print(sol_m1.T[0])
@@ -57,8 +73,8 @@ if __name__ == "__main__":
     # v_sol_m1 = sol_m1.T[0]
     # v_sol_m2 = sol_m2.y[0]
 
-    plt.plot(x, y1_sol)
-    plt.plot(x, y2_sol)
+    plt.plot(x, x_sol)
+    plt.plot(x, v_sol)
     # plt.ylabel('$v(t)$', fontsize=22)
     # plt.xlabel('$t$', fontsize=22)
     plt.show()
