3ee9816bcb9a047cb2c8f3af476642117a2cfb4f
Mark <doylethecat@gmail.com>
2024-11-05 19:37:51 -0500

    [CODE] Storage Start
    Initialized the h5py script into the grid search.

--- multi.py
+++ multi.py
@@ -11,6 +11,7 @@ print(f'imported')
 import main
 import os
 import datetime
+import h5py
 
 
 # Beta should go from 1 to 0.01
@@ -36,6 +37,24 @@ def open_directories(a, b):
     m.writelines(f'Starting trials: {datetime.datetime.now()} \n')
     return m, dir_name
 
+
+def read_data():
+    """
+    open the h5py data!
+    :return:
+    """
+    with h5py.File('experimental_results.h5', 'r') as f:
+        g_names = [name for name in f if isinstance(f[name], h5py.Group)]
+        group = f['param_1-0_1-0']
+        period_data = group['period'][:]
+        precision_data = group['precision'][:]
+        concentration_data = group['trial_0_concentrations']
+
+        print(f'period data: {period_data}')
+        print(f'names: {g_names}')
+
+
+
 def grid_run():
     """
     Runs a grid of parameters & attempts to solve
@@ -43,29 +62,60 @@ def grid_run():
     """
     betas = [10 ** (-1 * (i / 5)) for i in range(10 + 1)]
     alphas = [10 ** (i / 5) for i in range(10 + 1)]
-    trials_per_bin = 1000
-    for b in betas:
-        for a in alphas:
-            # Start by opening the directories
-            # m is our output.txt file writer!
-            m, dir_name = open_directories(a, b)
-            for n in range(trials_per_bin):
-                rt, rx, peaks, autoc = main.single_pass(b, a, 1, 100000)
-                if len(peaks) == 0:
-                    # Unable to find any peaks
-                    print(f'no peaks :(')
-                    period = 0
-                    precision = 0
-                else:
-                    # Found some peaks!
-                    print(f'periods: {rt[peaks]}')
-                    period = rt[peaks[0]]
-                    precision = autoc[peaks[0]]
-                m.writelines(f'{str(n).ljust(5)}: {period}, {precision} \n')
+    trials_per_bin = 3
+
+    with h5py.File('experimental_results.h5', 'w') as f:
+        for b in betas:
+            for a in alphas:
+                group_name = f'param_{str(b).replace(".", "-")}_{str(a).replace(".", "-")}'
+                group = f.create_group(group_name)
+
+                periods = []
+                precisions = []
+
+                for trial in range(trials_per_bin):
+                    rt, rx, peaks, autoc = main.single_pass(b, a, 1, 100000)
+                    group.create_dataset(f'trial_{trial}_concentrations', data=rx)
+                    if len(peaks) == 0:
+                        # Unable to find any peaks
+                        print(f'no peaks :(')
+                        period = 0
+                        precision = 0
+                    else:
+                        # Found some peaks!
+                        print(f'periods: {rt[peaks]}')
+                        period = rt[peaks[0]]
+                        precision = autoc[peaks[0]]
+                    periods.append(period)
+                    precisions.append(precision)
+
+                group.create_dataset('period', data=periods)
+                group.create_dataset('precision', data=precisions)
 
 
 
-            m.close()
+
+
+                # # Start by opening the directories
+                # # m is our output.txt file writer!
+                # m, dir_name = open_directories(a, b)
+                # for n in range(trials_per_bin):
+                #     rt, rx, peaks, autoc = main.single_pass(b, a, 1, 100000)
+                #     if len(peaks) == 0:
+                #         # Unable to find any peaks
+                #         print(f'no peaks :(')
+                #         period = 0
+                #         precision = 0
+                #     else:
+                #         # Found some peaks!
+                #         print(f'periods: {rt[peaks]}')
+                #         period = rt[peaks[0]]
+                #         precision = autoc[peaks[0]]
+                #     m.writelines(f'{str(n).ljust(5)}: {period}, {precision} \n')
+                #
+                #
+                #
+                # m.close()
         #     break
         # break
 
@@ -101,6 +151,7 @@ if __name__ == "__main__":
     # - Autocorrelation graph -
     # plt.plot(rt, autoc)
 
-    plt.show()
+    # plt.show()
 
-    grid_run()
+    read_data()
+    # grid_run()
